136a4aae7c4a39c45bf863d0dc204518b5c46a09
cesartidrick <cesartidrick@gmail.com>
2023-05-15 17:03:09 -0600

    everything else

--- src/images/textures.js
+++ src/images/textures.js
@@ -1,15 +1,36 @@
-import dirtImg from './dirt.jpg';
-import grassImg from './grass.jpg';
-import glassImg from './glass.png';
-import logImg from './log.jpg';
-import woodImg from './wood.png';
+import { NearestFilter, TextureLoader , RepeatWrapping} from 'three'
 
-const images = {
+
+import {
 	dirtImg,
 	grassImg,
 	glassImg,
 	woodImg,
 	logImg,
-}
+} from './images'
 
-export default images
+const dirtTexture = new TextureLoader().load(dirtImg)
+const grassTexture = new TextureLoader().load(grassImg)
+const glassTexture = new TextureLoader().load(glassImg)
+const woodTexture = new TextureLoader().load(woodImg)
+const logTexture = new TextureLoader().load(logImg)
+const groundTexture = new TextureLoader().load(grassImg)
+
+dirtTexture.magFilter = NearestFilter;
+logTexture.magFilter = NearestFilter;
+grassTexture.magFilter = NearestFilter;
+glassTexture.magFilter = NearestFilter;
+woodTexture.magFilter = NearestFilter;
+groundTexture.magFilter = NearestFilter;
+
+groundTexture.wrapS = RepeatWrapping
+groundTexture.wrapT = RepeatWrapping
+
+export {
+	dirtTexture,
+	grassTexture,
+	glassTexture,
+	woodTexture,
+	logTexture,
+	groundTexture
+}
